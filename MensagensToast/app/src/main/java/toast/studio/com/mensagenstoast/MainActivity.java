af8b48f5d53c22fc022072230e6b56cb4358f732
Hercules Lima <hl3058317@gmail.com>
2018-08-05 20:22:50 -0400

    Mensagens Toast

---
+++ MensagensToast/app/src/main/java/toast/studio/com/mensagenstoast/MainActivity.java
@@ -0,0 +1,29 @@
+package toast.studio.com.mensagenstoast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button botao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        botao = (Button) findViewById(R.id.botaoId);
+
+        botao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Você clicou no botão!",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
