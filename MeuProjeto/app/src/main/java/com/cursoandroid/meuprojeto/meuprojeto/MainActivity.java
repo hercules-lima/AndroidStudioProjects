0299300f08034c60794692e8b929dd563be65cb1
Hercules Lima <hl3058317@gmail.com>
2018-08-15 13:53:08 -0400

    Firebase - Salvando

--- MeuProjeto/app/src/main/java/com/cursoandroid/meuprojeto/meuprojeto/MainActivity.java
+++ MeuProjeto/app/src/main/java/com/cursoandroid/meuprojeto/meuprojeto/MainActivity.java
@@ -2,19 +2,27 @@ package com.cursoandroid.meuprojeto.meuprojeto;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.cursoandroid.meuprojeto.meuprojeto.Usuario;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
-    private DatabaseReference firebaseReferencia = FirebaseDatabase.getInstance().getReference();
+    private DatabaseReference databaseReferencia = FirebaseDatabase.getInstance().getReference();
+    private DatabaseReference usuarioReferencia = databaseReferencia.child("usuarios");
+    private DatabaseReference produtoReferencia = databaseReferencia.child("produtos");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //caso não existir cria um nó, se existir sobreescreve
+        Produto produto = new Produto();
+        produto.setCor("Azul");
+        produto.setDescricao("Rato");
+        produto.setFabricante("Samsung");
 
-        firebaseReferencia.child("menininha").setValue("Thuani");
+        produtoReferencia.child("001").setValue(produto);
     }
 }
