d7d30468207cf562ff345dc06a433e5b5aefe16b
Hercules Lima <hl3058317@gmail.com>
2018-08-15 19:52:24 -0400

    WHATSAPP - TELA LOGIN VALIDAÇÃO P2

--- WhatsApp/app/src/main/java/br/com/whatsappandroid/cursoandroid/whatsapp/activity/MainActivity.java
+++ WhatsApp/app/src/main/java/br/com/whatsappandroid/cursoandroid/whatsapp/activity/MainActivity.java
@@ -1,10 +1,12 @@
-package br.com.whatsappandroid.cursoandroid.whatsapp;
+package br.com.whatsappandroid.cursoandroid.whatsapp.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import br.com.whatsappandroid.cursoandroid.whatsapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
