c746e952bf7576e1c023cfa737b7277239929845
Hercules Lima <hl3058317@gmail.com>
2018-08-15 12:32:49 -0400

    Firebase - Configuração

---
+++ MeuProjeto/app/src/main/java/com/cursoandroid/meuprojeto/meuprojeto/MainActivity.java
@@ -0,0 +1,20 @@
+package com.cursoandroid.meuprojeto.meuprojeto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference firebaseReferencia = FirebaseDatabase.getInstance().getReference();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        firebaseReferencia.child("menininha").setValue("Thuani");
+    }
+}
