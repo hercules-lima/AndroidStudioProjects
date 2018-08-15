3cf168cd32efc3663bd837a9903e838e541f2598
Hercules Lima <hl3058317@gmail.com>
2018-08-15 14:09:35 -0400

    Firebase - Recuperando

--- MeuProjeto/app/src/main/java/com/cursoandroid/meuprojeto/meuprojeto/MainActivity.java
+++ MeuProjeto/app/src/main/java/com/cursoandroid/meuprojeto/meuprojeto/MainActivity.java
@@ -2,21 +2,27 @@ package com.cursoandroid.meuprojeto.meuprojeto;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
 import com.cursoandroid.meuprojeto.meuprojeto.Usuario;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
     private DatabaseReference databaseReferencia = FirebaseDatabase.getInstance().getReference();
     private DatabaseReference usuarioReferencia = databaseReferencia.child("usuarios");
-    private DatabaseReference produtoReferencia = databaseReferencia.child("produtos");
+    private DatabaseReference produtoReferencia = databaseReferencia.child("produtos").child("001");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*
         //caso não existir cria um nó, se existir sobreescreve
         Produto produto = new Produto();
         produto.setCor("Azul");
@@ -24,5 +30,19 @@ public class MainActivity extends AppCompatActivity {
         produto.setFabricante("Samsung");
 
         produtoReferencia.child("001").setValue(produto);
+        */
+
+        usuarioReferencia.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Log.i("FIREBASE",dataSnapshot.getValue().toString() );
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
     }
 }
