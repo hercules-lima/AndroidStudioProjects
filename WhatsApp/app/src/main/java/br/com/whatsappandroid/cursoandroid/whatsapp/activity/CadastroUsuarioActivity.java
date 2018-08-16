b8fa837719feb0c714fcc457047ff82f08c93fff
Hercules Lima <hl3058317@gmail.com>
2018-08-16 15:10:44 -0400

    WHATSAPP - CADASTRO DE USUARIO P2

--- WhatsApp/app/src/main/java/br/com/whatsappandroid/cursoandroid/whatsapp/activity/CadastroUsuarioActivity.java
+++ WhatsApp/app/src/main/java/br/com/whatsappandroid/cursoandroid/whatsapp/activity/CadastroUsuarioActivity.java
@@ -1,12 +1,20 @@
 package br.com.whatsappandroid.cursoandroid.whatsapp.activity;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import br.com.whatsappandroid.cursoandroid.whatsapp.R;
+import br.com.whatsappandroid.cursoandroid.whatsapp.config.ConfiguracaoFirebase;
 import br.com.whatsappandroid.cursoandroid.whatsapp.model.Usuario;
 
 public class CadastroUsuarioActivity extends AppCompatActivity {
@@ -15,6 +23,7 @@ public class CadastroUsuarioActivity extends AppCompatActivity {
     private EditText senha;
     private Button botaoCadastrar;
     private Usuario usuario;
+    private FirebaseAuth autenticacao;
 
 
     @Override
@@ -23,7 +32,7 @@ public class CadastroUsuarioActivity extends AppCompatActivity {
         setContentView(R.layout.activity_cadastro_usuario);
 
         nome = (EditText) findViewById(R.id.editCadastroNome);
-        email = (EditText) findViewById(R.id.edit_login_email);
+        email = (EditText) findViewById(R.id.editCadastroEmail);
         senha = (EditText) findViewById(R.id.editCadastroSenha);
         botaoCadastrar = (Button) findViewById(R.id.bt_cadastrar);
 
@@ -42,6 +51,19 @@ public class CadastroUsuarioActivity extends AppCompatActivity {
     }
 
     public void cadastrarUsuario(){
-        
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+        autenticacao.createUserWithEmailAndPassword(
+                usuario.getEmail(),
+                usuario.getSenha()
+        ).addOnCompleteListener(CadastroUsuarioActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(CadastroUsuarioActivity.this,"SUCESSO AO CADASTRAR USUÁRIO!",Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(CadastroUsuarioActivity.this,"ERRO AO CADASTRAR USUÁRIO!",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
--- WhatsApp/app/src/main/java/br/com/whatsappandroid/cursoandroid/whatsapp/config/ConfiguracaoFirebase.java
+++ WhatsApp/app/src/main/java/br/com/whatsappandroid/cursoandroid/whatsapp/config/ConfiguracaoFirebase.java
@@ -5,6 +5,7 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -15,11 +16,19 @@ import br.com.whatsappandroid.cursoandroid.whatsapp.R;
 public final class ConfiguracaoFirebase extends AppCompatActivity {
 
     private static DatabaseReference referenciaFirebase;
+    private static FirebaseAuth autenticacao;
 
     public static DatabaseReference getFirebase(){
         if(referenciaFirebase == null) {
             referenciaFirebase = FirebaseDatabase.getInstance().getReference();
         }
         return referenciaFirebase;
+    }
+
+    public static FirebaseAuth getFirebaseAutenticacao(){
+        if(autenticacao == null) {
+            autenticacao = FirebaseAuth.getInstance();
+        }
+        return autenticacao;
     }
 }
