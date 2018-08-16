85041d022c539b85f6f4a66e77c24c177b2dbc5d
Hercules Lima <hl3058317@gmail.com>
2018-08-16 14:33:29 -0400

    WHATSAPP - CADASTRO DE USUARIO

--- WhatsApp/app/src/main/java/br/com/whatsappandroid/cursoandroid/whatsapp/activity/CadastroUsuarioActivity.java
+++ WhatsApp/app/src/main/java/br/com/whatsappandroid/cursoandroid/whatsapp/activity/CadastroUsuarioActivity.java
@@ -2,14 +2,46 @@ package br.com.whatsappandroid.cursoandroid.whatsapp.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import br.com.whatsappandroid.cursoandroid.whatsapp.R;
+import br.com.whatsappandroid.cursoandroid.whatsapp.model.Usuario;
 
 public class CadastroUsuarioActivity extends AppCompatActivity {
+    private EditText nome;
+    private EditText email;
+    private EditText senha;
+    private Button botaoCadastrar;
+    private Usuario usuario;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_usuario);
+
+        nome = (EditText) findViewById(R.id.editCadastroNome);
+        email = (EditText) findViewById(R.id.edit_login_email);
+        senha = (EditText) findViewById(R.id.editCadastroSenha);
+        botaoCadastrar = (Button) findViewById(R.id.bt_cadastrar);
+
+        botaoCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                usuario = new Usuario();
+                usuario.setNome(nome.getText().toString());
+                usuario.setEmail(email.getText().toString());
+                usuario.setSenha(senha.getText().toString());
+
+                cadastrarUsuario();
+
+            }
+        });
+    }
+
+    public void cadastrarUsuario(){
+        
     }
 }
---
+++ WhatsApp/app/src/main/java/br/com/whatsappandroid/cursoandroid/whatsapp/model/Usuario.java
@@ -0,0 +1,53 @@
+package br.com.whatsappandroid.cursoandroid.whatsapp.model;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import br.com.whatsappandroid.cursoandroid.whatsapp.R;
+import br.com.whatsappandroid.cursoandroid.whatsapp.config.ConfiguracaoFirebase;
+
+public class Usuario {
+    private String nome;
+    private String id;
+    private String email;
+    private String senha;
+
+    public Usuario(){
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
